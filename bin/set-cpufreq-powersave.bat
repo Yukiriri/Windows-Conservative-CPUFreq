6fbceb609210554dc78f09f46531799a30e2923b
Yukiriri <50292962+Yukiriri@users.noreply.github.com>
2025-12-30 11:31:53 +0800

    修正实现powersave

--- bin/set-cpufreq-powersave.bat
+++ bin/set-cpufreq-powersave.bat
@@ -1,20 +1,23 @@
 for %%i in (SetAcValueIndex SetDcValueIndex) do (
     powercfg -%%i Scheme_Current Sub_Processor PerfAutonomous 0
-    powercfg -%%i Scheme_Current Sub_Processor PerfCheck 20
+    powercfg -%%i Scheme_Current Sub_Processor PerfCheck 10
 
-    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin 0
-    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin1 0
-    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin2 0
+    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin 100
+    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin1 100
+    powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMin2 100
     powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMax 100
     powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMax1 100
     powercfg -%%i Scheme_Current Sub_Processor ProcThrottleMax2 100
+    powercfg -%%i Scheme_Current Sub_Processor ProcFreqMax 1
+    powercfg -%%i Scheme_Current Sub_Processor ProcFreqMax1 1
+    powercfg -%%i Scheme_Current Sub_Processor ProcFreqMax2 1
 
     powercfg -%%i Scheme_Current Sub_Processor PerfBoostMode 0
     @REM powercfg -%%i Scheme_Current Sub_Processor PerfBoostPol
-    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold 99
-    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold1 99
-    powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold 90
-    powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold1 90
+    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold 0
+    powercfg -%%i Scheme_Current Sub_Processor PerfIncThreshold1 0
+    powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold 0
+    powercfg -%%i Scheme_Current Sub_Processor PerfDecThreshold1 0
     powercfg -%%i Scheme_Current Sub_Processor PerfIncPol 1
     powercfg -%%i Scheme_Current Sub_Processor PerfIncPol1 1
     powercfg -%%i Scheme_Current Sub_Processor PerfDecPol 2
